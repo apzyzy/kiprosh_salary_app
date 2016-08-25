72e52bb030d0ed1049b1e20b0f5270d94f415135
Varun Lalan <varun.lalan@kiprosh.com>
2016-08-25 21:22:00 +0530

    Minor improvements in Admin pages

--- app/admin/associate.rb
+++ app/admin/associate.rb
@@ -2,6 +2,8 @@ ActiveAdmin.register Associate do
   permit_params :associate_number, :designation, :email, :full_name,
                 :pan_number, :title
 
+  config.sort_order = 'associate_number_asc'
+
   index do
     column :associate_number
     column :title
