ff9cfba4acf15d602f67df140ac2475be2a7f063
Varun Lalan <varun.lalan@kiprosh.com>
2016-09-02 20:28:32 +0530

    Small UI and textual fixes

--- app/mailers/accounts_mailer.rb
+++ app/mailers/accounts_mailer.rb
@@ -1,9 +1,7 @@
 class AccountsMailer < ApplicationMailer
   add_template_helper(ApplicationHelper)
 
-  default from: 'do-not-reply@kiprosh.com',
-          to: Settings.mailer.test_accounts_email || 'accounts@kiprosh.com',
-          cc: Settings.mailer.cc_accounts_email
+  default from: 'do-not-reply@kiprosh.com'
 
   def incorrect_calculation_email(obj)
     @obj = obj
@@ -11,6 +9,15 @@ class AccountsMailer < ApplicationMailer
     subject = "CONCERN - Calculation issues while processing pay slip for #{name}"
 
     attachments["#{name}.pdf"] = attach_pay_slip(obj)
-    mail(subject: subject)
+    mail(
+      to: accounts_email,
+      cc: Settings.mailer.cc_accounts_email,
+      subject: subject)
+  end
+
+  private
+
+  def accounts_email
+    Settings.mailer.test_accounts_email || 'accounts@kiprosh.com'
   end
 end
